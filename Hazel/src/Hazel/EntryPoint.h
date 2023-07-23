3ad9d8f2b9997a23f59a862c4853161e8e0117f6
--global <--global>
2023-07-23 14:22:44 +0400

    Added log support.

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include "Log.h"
+
 #ifdef HZ_PLATFORM_WINDOWS
 
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+
 	Hazel::Application* app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
